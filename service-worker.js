d78b31cbaa13437dfce58c4ae0b0bad6d60528b8
ysys-park <tsman1@naver.com>
2026-04-19 22:53:45 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='solar-sales-final-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./service-worker.js'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));});
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));});
