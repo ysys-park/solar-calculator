477e79b6120e212a6cb2e37ea7b5c9d9800971d8
ysys-park <tsman1@naver.com>
2026-04-19 21:50:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
-const CACHE_NAME='solar-calculator-records-pwa-v1';
+const CACHE_NAME='solar-quote-layout-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./service-worker.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
 });
 self.addEventListener('fetch',event=>{
   event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
